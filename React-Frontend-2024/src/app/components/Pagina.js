a213ae71d9ab0fe80bce414dec12846a59568af8
Gustavo Henrique <gustavo.h.lima@iesb.edu.br>
2024-09-09 15:06:34 -0300

    Atividade-5

--- React-Frontend-2024/src/app/components/Pagina.js
+++ React-Frontend-2024/src/app/components/Pagina.js
@@ -1,23 +1,37 @@
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { Container, Nav, NavDropdown, Navbar } from "react-bootstrap";
 
 export default function Pagina(props) {
+
     return (
         <>
             <Navbar bg="dark" data-bs-theme="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="/filmes">Filmes Revisão</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        <Nav.Link href="/filmes">Filmes Revisão</Nav.Link>
+                        <Nav.Link href="/filmes">Filmes</Nav.Link>
+                        <Nav.Link href="/series">Series</Nav.Link>
+                        <Nav.Link href="/atores">Atores</Nav.Link>
+                        <NavDropdown title="Séries" id="basic-nav-dropdown"> 
+                            <NavDropdown.Item href="/series">
+                                Populares
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Filmes" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="./filmes">
+                                Em cartaz
+                            </NavDropdown.Item>
+                            
+                            <NavDropdown.Divider />
+
+                        </NavDropdown>
                     </Nav>
                 </Container>
             </Navbar>
 
-            <div className="bg-secondary text-white text-center p-3">
+            <div className="bg-white text-black text-center p-3">
                 <h1>{props.titulo}</h1>
             </div>
-
             <Container>
                 {props.children}
             </Container>
